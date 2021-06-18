560ecb828753dd9ef487b6dd27289671e95c8c9d
Zack <zack@ukaton.com>
2021-06-17 20:56:29 -0700

    updated for current sdk/firmware

--- script/script.js
+++ script/script.js
@@ -3,11 +3,14 @@
 window.addEventListener("message", event => {
     const {data} = event
     if (data.ukaton === "sideMission") {
-        const {index, type, timestamp} = data
+        const {type, timestamp} = data
 
         let value
         switch(type) {
             case "acceleration":
+                value = new THREE.Vector3().set(...data.value)
+                break
+            case "gravity":
                 value = new THREE.Vector3().set(...data.value)
                 break
             case "linearAcceleration":
@@ -16,17 +19,24 @@ window.addEventListener("message", event => {
             case "rotationRate":
                 value = new THREE.Euler().set(...data.value)
                 break
-            case "euler":
-                value = new THREE.Euler().set(...data.value)
+            case "magnetometer":
+                value = new THREE.Vector3().set(...data.value)
                 break
             case "quaternion":
                 value = new THREE.Quaternion().set(...data.value)
+                break
+            case "euler":
+                value = new THREE.Euler().set(...data.value)
+                break
+
+            case "callibratedEuler":
+                value = new THREE.Euler().set(...data.value)
                 break
         }
 
         if (value) {
             window.dispatchEvent(new CustomEvent("sidemission", {
-                detail: {index, type, value, timestamp}
+                detail: {type, value, timestamp}
             }))
         }
     }
@@ -38,22 +48,12 @@ modelViewers.forEach(modelViewer => {
     modelViewer.setAttribute("interaction-prompt", "none")
 })
 
-let yawOffset = 0
-let currentYaw = 0
-
-document.addEventListener("keypress", event => {
-    if (event.key === "c") {
-        yawOffset = currentYaw
-    }
-})
-
 window.addEventListener("sidemission", event => {
     const {type, value}  = event.detail
-    if (type === "euler") {
+    if (type === "callibratedEuler") {
         const euler = value
         modelViewers.forEach(modelViewer => {
-            currentYaw = euler.y
-            modelViewer.setAttribute("camera-orbit", `${euler.y - yawOffset}rad ${euler.x+(Math.PI/2)}rad`);
+            modelViewer.setAttribute("camera-orbit", `${euler.y}rad ${euler.x+(Math.PI/2)}rad`);
         })
     }
 })
